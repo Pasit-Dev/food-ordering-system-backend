d9dfe06101cb18ba8cf8be32ba97d17c8c31c234
Pasit <pasitgut.dev@gmail.com>
2025-03-10 21:16:57 +0700

    fix bug

--- src/resetDatabase.js
+++ src/resetDatabase.js
@@ -5,7 +5,7 @@ export async function resetDatabase() {
     const client = await pool.connect()
     try {
 
-        const resetSQL = `TRUNCATE TABLE Menus, MenuGroups, MenuOptions, Orders, OrderItems, OrderItemOptions, OrderItemHistory RESTART INDENTITY CASCADE;`;
+        const resetSQL = `TRUNCATE TABLE Menus, MenuGroups, MenuOptions, Orders, OrderItems, OrderItemOptions, OrderItemHistory RESTART IDENTITY CASCADE;`;
         await client.query(resetSQL);
 
         console.log('Database reset successfully');
